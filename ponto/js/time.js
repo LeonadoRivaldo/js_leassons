299d68344c24499a7cdd3a38f95f384d1bc8682e
Leonardo Rivaldo <leonardo.rivaldo@gmail.com>
2017-04-08 17:55:42 -0300

    doing old school shit

--- ponto/js/time.js
+++ ponto/js/time.js
@@ -1,7 +1,22 @@
 var MyDate = function( date ){
+
+	function tag(strings, ...values){
+		if( values[0] < 10 ){
+			values[0] = "0"+values[0]; 
+		}
+		if( values[1] < 10 ){
+			values[1] = "0"+values[1]; 
+		}
+		if( values[2] < 10 ){
+			values[2] = "0"+values[2]; 
+		}
+		return `${values[0]}:${values[1]}:${values[2]}`;
+	}
+
+
 	this.date = new Date( date );
 	var timeString = function() {
-		return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+		return tag`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 	};
 	return{
 		data: this.date,
