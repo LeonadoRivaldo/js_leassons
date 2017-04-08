e3fbdb2d4189118e5a14f93bab1a7a5f4902ab77
Leonardo Rivaldo <leonardo.rivaldo@gmail.com>
2017-04-08 17:01:24 -0300

    fazendo tudo vanila

--- ponto/js/app.js
+++ ponto/js/app.js
@@ -1,9 +1,16 @@
-function registrar(query) {
+function registrar(query, elem) {
+	var me = elem;
+	var meChild = me.firstElementChild;
+	me.removeAttribute("onClick");
+	me.setAttribute('tip', "Ponto já registrado!")
+	me.className = me.className + " default-pointer"
+	meChild.textContent = "done"; 
 	var data = new MyDate( new Date() );
 	var aviso = new Notify();
 	if( query.split(" ")[0] == "#chegada" ){
 		aviso.chegada(data);
 	}
+
 	var elemento = document.querySelector(query);
 	elemento.innerHTML = data.toString(); 
 }
